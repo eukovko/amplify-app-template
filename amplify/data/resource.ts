d443a66c6d7395b6becdac6a1bca5d8ef767a9c3
Eugene Kovko <eu.kovko@gmail.com>
2026-02-09 21:40:19 +0100

    fix: Fixed the schema

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,6 +1,12 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-const schema = a.schema({});
+const schema = a.schema({
+  Placeholder: a
+    .model({
+      id: a.id(),
+    })
+    .authorization((allow) => [allow.authenticated()]),
+});
 
 export type Schema = ClientSchema<typeof schema>;
 
